2a1b3ffa7de2751394df8a5bab2caa1a7fb4da7d
itheima <270761120@qq.com>
2020-08-24 17:35:17 +0800

    注册登录完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,7 @@ $(function () {
 
     //从layui中获取form对象
     var form = layui.form
+    var  layer = layui.layer
     //通过form.verify()函数自定义检验规则
     form.verify({
         //自定义了一个叫做pwd的校验规则
@@ -31,19 +32,47 @@ $(function () {
 
 
     //监听注册表单的提交事件
+
     $('#form_reg').on('submit', function (e) {
+        var data = {
+            username: $('#form_reg [name = username]').val(),               
+            password: $('#form_reg [name = password]').val()
+        }
         e.preventDefault()
-        $.post('http://ajax.frontend.itheima.net/api/reguser',
-            {
-                username: $('#form_reg [name = username]').val(),
-               
-                password: $('#form_reg [name = password]').val()
-            },
+        $.post('/api/reguser',
+            data,
             function (res) {
                 if(res.status !== 0) {
-                    return console.log(res.message);
+                    // return console.log(res.message);
+                     return layer.msg(res.message);
                 }
-                alert("注册成功");
+                layer.msg("注册成功,请登录");
+                //调用登录
+                $('#link_login').click()
             })
     })
+
+
+    //监听登录表彰的提交事件
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+           url: '/api/login',
+           method: 'POST',
+           data: $(this).serialize(),
+           success: function(res) {
+               if(res.status !==0) {
+                   return layer.msg('登录失败')
+               }
+               layer.msg('登录成功')
+            //    将登录成功得到的字符串保存到localStorage
+            localStorage.setItem('token',res.token)
+               console.log(res.token);
+               location.href = '/index.html'
+           }
+        })
+    })
+
+
+
 })
